ea862e463fa75a76bb6d83be635356c3f302b91a
Rasheen Casey <rdc26631@email.vccs.edu>
2026-05-07 14:17:36 -0400

    Remove unused folders

--- rcaseymod7Assignment/src/main/java/edu/vwcc/mod7/AccountNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package edu.vwcc.mod7;
-
-public class AccountNotFoundException extends RuntimeException{
-
-}
--- rcaseymod7Assignment/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO account (name, amount) VALUES ('Jane Down', 1000);
-INSERT INTO account (name, amount) VALUES ('John Smith', 1000);
